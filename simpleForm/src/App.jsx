54b040e2976725495cbbf38d9a27652cb8b07b94
Archana Ojha <archana.ozza@gmail.com>
2024-05-30 23:50:34 +0300

    assign state variable and use them to set value of input field

--- simpleForm/src/App.jsx
+++ simpleForm/src/App.jsx
@@ -1,10 +1,55 @@
 import Form from "./components/form"
+import {useState} from "react"
+
 function App() {
+  const [fullName, setfullName] = useState('')
+  const [date, setDate] = useState('')
+  const [city, setCity] = useState('')
+  const [streetAd, setStreetAd] = useState('')
+  const [number, setNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
  
+  //function for the onchange method 
+  const nameChange = (event) => {
+    setfullName(event.target.value)
+
+  }
+
+  const dobChange = (event) => {
+    setDate(event.target.value)
+
+  }
+  const cityChange = (event) => {
+    setCity(event.target.value)
+
+  }
+  const streetChange = (event) => {
+    setStreetAd(event.target.value)
+
+  }
+  const numberChange = (event) => {
+    setNumber(event.target.value)
+    
+  }
+  const emailChange = (event) => {
+    setEmail(event.target.value)
+    
+  }
+  //for form submit
+  const submitForm = (event) => {
+    event.preventDefault()
+    setIsSubmitted(true);
+
+  }
 
   return (
     <>
-      <Form/>
+      <Form fullName={fullName} date={date} city={city} streetAd={streetAd}
+      number={number} email={email} nameChange={nameChange} dobChange={dobChange} submitForm={submitForm}
+      cityChange={cityChange} streetChange={streetChange} numberChange={numberChange} emailChange={emailChange} isSubmitted={isSubmitted} />
+
     </>
   )
 }
--- simpleForm/src/components/form.jsx
+++ simpleForm/src/components/form.jsx
@@ -1,36 +1,37 @@
-const Form = () => {
+const Form = ({fullName, date, city, number, streetAd, email, nameChange, dobChange, cityChange, streetChange, numberChange, emailChange, submitForm, isSubmitted}) => {
     return(
         <div>
-            <form>
+            <form onSubmit={submitForm}>
                 <fieldset>
                     <legend>Personal Info</legend>
                     <div>
-                    FullName: <input/>
+                    FullName: <input value = {fullName} onChange={nameChange}/>
                     </div>
                     <div>
-                    Date of Birth: <input />
+                    Date of Birth: <input value = {date} onChange={dobChange} />
                     </div>
                     <div>
                     country: <select><option>...</option></select>
                     </div>
                     <div>
-                    city: <input/>
+                    city: <input value = {city} onChange={cityChange}/>
                     </div>
                     <div>
-                    Street Address: <input/>
+                    Street Address: <input value = {streetAd} onChange={streetChange}/>
                     </div>
                     <div>
-                    Mobile Number: <input/>
+                    Mobile Number: <input value = {number} onChange={numberChange}/>
                     </div>
                     <div>
-                    Email: <input type="email" />
+                    Email: <input type="email" value = {email} onChange={emailChange} />
                     </div>
                     <div>
                     gender: <br /> <input type="radio"/> <label >male</label>
                     <input type="radio" /> <label>female</label>
                     <input type="radio" /> <label>other</label>
                     </div>
-                    <button>Submit</button>
+                    <button type="submit">Submit</button>
+                    {isSubmitted ? <p>Thank you for submitting form</p> : <p>Please submit the form</p>}
                     
                 </fieldset>
             </form>
